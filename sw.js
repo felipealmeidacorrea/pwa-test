a3cf12a7073b5970778ff381d60e7eeb77299def
Felipe Almeida Correa <felipealmeidacorrea@outlook.com.br>
2022-07-13 16:22:10 -0400

    service worker test

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll([
-                'pwa-test/',
+                '/',
                 '/index.html',
                 'styles/style.css',
             ]);
